249ad0d421f262b561b6c17b41e86de9edb48371
Milad Farazian <mfarazia@ucsc.edu>
2024-09-25 15:34:21 -0700

    Resolve PApplet issues on macOS

--- JavaProcessing/src/BouncingBox.java
+++ JavaProcessing/src/BouncingBox.java
@@ -1,6 +1,3 @@
-import processing.core.PApplet;
-
-
 public class BouncingBox extends MyFirstProject {
     public int boxX;
     public int boxY;
@@ -10,23 +7,25 @@ public class BouncingBox extends MyFirstProject {
     public int boxHeight;
     public float gravity;
     public boolean onFloor;
+
     public BouncingBox() {
         boxX = Math.round(random(0, 500));
         boxY = Math.round(random(0, 200));
-        if(random(0,1) < 0.5) {
-            velX = random(1.5f,5);
+        if (random(0, 1) < 0.5) {
+            velX = random(1.5f, 5);
         } else {
-            velX = random(-5,-1.5f);
+            velX = random(-5, -1.5f);
         }
-        velY = random(-10,-5);
-        boxWidth = Math.round(random(20,80));
-        boxHeight = Math.round(random(20,80));
-        gravity = random(0.05f,0.5f);
+        velY = random(-10, -5);
+        boxWidth = Math.round(random(20, 80));
+        boxHeight = Math.round(random(20, 80));
+        gravity = random(0.05f, 0.5f);
         onFloor = false;
 
     }
+
     public BouncingBox(int x, int y, float xvel, float yvel, int width, int height, float gravityY) {
-        boxX =  x;
+        boxX = x;
         boxY = y;
         velX = xvel;
         velY = yvel;
@@ -35,36 +34,40 @@ public class BouncingBox extends MyFirstProject {
         gravity = gravityY;
         onFloor = false;
     }
+
     public void updateBox() {
         this.boxX += this.velX;
-        if(this.boxX + this.boxWidth > 500) {
+        if (this.boxX + this.boxWidth > 500) {
             this.boxX = 500 - this.boxWidth;
             this.velX = Math.abs(this.velX) * -1;
         }
-        if(this.boxX < 0) {
+        if (this.boxX < 0) {
             this.boxX = 0;
             this.velX = Math.abs(this.velX);
         }
         this.boxY += this.velY;
         this.onFloor = false;
-        if(this.boxY + this.boxHeight > 500) {
+        if (this.boxY + this.boxHeight > 500) {
             this.boxY = 500 - this.boxHeight;
             this.velY = Math.abs(this.velY) * -1;
             this.onFloor = true;
         }
-        if(this.boxY < 0) {
+        if (this.boxY < 0) {
             this.boxY = 0;
             this.velY = Math.abs(this.velY) * 0.5f;
         }
         velY += gravity;
     }
+
     public boolean touchingPlayer(int playerX) {
-        if(( (playerX + 30 < this.boxX + this.boxWidth && playerX +30 > this.boxX) || (playerX < this.boxX + this.boxWidth && playerX > this.boxX))  && this.boxY + this.boxHeight > 470) {
+        if (((playerX + 30 < this.boxX + this.boxWidth && playerX + 30 > this.boxX)
+                || (playerX < this.boxX + this.boxWidth && playerX > this.boxX)) && this.boxY + this.boxHeight > 470) {
             return (true);
         } else {
             return (false);
         }
     }
+
     public boolean shouldShake() {
         return onFloor;
     }
--- JavaProcessing/src/MyFirstProject.java
+++ JavaProcessing/src/MyFirstProject.java
@@ -1,9 +1,7 @@
 import processing.core.PApplet;
 import processing.core.PFont;
-import java.util.concurrent.TimeUnit;
 
-
-public class MyFirstProject extends PApplet{
+public class MyFirstProject extends PApplet {
     PFont font;
     public BouncingBox box1;
     public BouncingBox box2;
@@ -27,12 +25,13 @@ public class MyFirstProject extends PApplet{
     public float scoreSlideY;
     public float score2SlideY;
     public float menuButtonSlideY;
+
     public void settings() {
-        size (500, 500);
+        size(500, 500);
     }
 
     public void setup() {
-        background(40,50,140);
+        background(40, 50, 140);
         font = createFont("Impact", 16, true);
         String[] fontList = PFont.list();
 
@@ -57,11 +56,9 @@ public class MyFirstProject extends PApplet{
 
     }
 
-
-
     public void draw() {
-        //frame.setLocation((int)score * 20, 0);
-        if(MODE == "MENU") {
+        // frame.setLocation((int)score * 20, 0);
+        if (MODE == "MENU") {
             background(255, 255, 255);
             noStroke();
 
@@ -70,20 +67,21 @@ public class MyFirstProject extends PApplet{
                 playerX = 470;
             }
 
-            //Boxes
+            // Boxes
             demoBox1.updateBox();
             demoBox2.updateBox();
             demoBox3.updateBox();
             demoBox4.updateBox();
             demoBox5.updateBox();
             demoBox6.updateBox();
-            shake = demoBox1.shouldShake() || demoBox2.shouldShake() || demoBox3.shouldShake() || demoBox4.shouldShake() || demoBox5.shouldShake() || demoBox6.shouldShake();
-            if(shake) {
+            shake = demoBox1.shouldShake() || demoBox2.shouldShake() || demoBox3.shouldShake() || demoBox4.shouldShake()
+                    || demoBox5.shouldShake() || demoBox6.shouldShake();
+            if (shake) {
                 pushMatrix();
-                translate(random(-5,5), random(-5,5));
+                translate(random(-5, 5), random(-5, 5));
             }
-            fill(0,0,0);
-            rect(0,0,width,height);
+            fill(0, 0, 0);
+            rect(0, 0, width, height);
 
             fill(255, 255, 255);
             rect(demoBox1.boxX, demoBox1.boxY, demoBox1.boxWidth, demoBox1.boxHeight);
@@ -97,27 +95,27 @@ public class MyFirstProject extends PApplet{
             textAlign(CENTER);
             textFont(font, 150);
             text("BOUND", 250, 150);
-            if(mouseX < 300 && mouseX > 200 && mouseY > 355 && mouseY < 405) {
-                fill(255,255,255);
-                rect(200,355,100,50);
-                fill(0,0,0);
+            if (mouseX < 300 && mouseX > 200 && mouseY > 355 && mouseY < 405) {
+                fill(255, 255, 255);
+                rect(200, 355, 100, 50);
+                fill(0, 0, 0);
                 textFont(font, 50);
                 text("PLAY", 250, 400);
-                if(mousePressed) {
+                if (mousePressed) {
                     MODE = "PLAYING";
                     delay(500);
                 }
             } else {
-                fill(255,255,255);
+                fill(255, 255, 255);
                 textFont(font, 50);
                 text("PLAY", 250, 400);
             }
 
-            if(shake) {
+            if (shake) {
                 popMatrix();
             }
         }
-        if(MODE == "PLAYING") {
+        if (MODE == "PLAYING") {
 
             // Background
             background(255, 255, 255);
@@ -128,17 +126,18 @@ public class MyFirstProject extends PApplet{
             if (playerX + 30 > 500) {
                 playerX = 470;
             }
-            //Boxes
-            if(box1.touchingPlayer(playerX) || box2.touchingPlayer(playerX) || box2.touchingPlayer(playerX) || box3.touchingPlayer(playerX) || box4.touchingPlayer(playerX)) {
+            // Boxes
+            if (box1.touchingPlayer(playerX) || box2.touchingPlayer(playerX) || box2.touchingPlayer(playerX)
+                    || box3.touchingPlayer(playerX) || box4.touchingPlayer(playerX)) {
                 MODE = "PAUSE";
             }
-            if(score > 5) {
-                if(box5.touchingPlayer(playerX)) {
+            if (score > 5) {
+                if (box5.touchingPlayer(playerX)) {
                     MODE = "PAUSE";
                 }
             }
             if (score > 10) {
-                if(box6.touchingPlayer(playerX)) {
+                if (box6.touchingPlayer(playerX)) {
                     MODE = "PAUSE";
                 }
             }
@@ -149,83 +148,82 @@ public class MyFirstProject extends PApplet{
 
             shake = box1.shouldShake() || box2.shouldShake() || box3.shouldShake() || box4.shouldShake();
 
-            if(score > 10) {
+            if (score > 10) {
                 box6.updateBox();
-                if(box6.shouldShake()) {
+                if (box6.shouldShake()) {
                     shake = true;
                 }
             }
-            if(score > 5) {
+            if (score > 5) {
                 box5.updateBox();
-                if(box5.shouldShake()) {
+                if (box5.shouldShake()) {
                     shake = true;
                 }
             }
 
-            if(shake) {
+            if (shake) {
                 pushMatrix();
-                translate(random(-5,5), random(-5,5));
+                translate(random(-5, 5), random(-5, 5));
             }
 
-            fill(0,0,0);
-            rect(0,0,width,height);
+            fill(0, 0, 0);
+            rect(0, 0, width, height);
             fill(255, 255, 255);
             rect(playerX, 470, 30, 30);
             rect(box1.boxX, box1.boxY, box1.boxWidth, box1.boxHeight);
             rect(box2.boxX, box2.boxY, box2.boxWidth, box2.boxHeight);
             rect(box3.boxX, box3.boxY, box3.boxWidth, box3.boxHeight);
             rect(box4.boxX, box4.boxY, box4.boxWidth, box4.boxHeight);
-            if(score > 5) {
+            if (score > 5) {
                 rect(box5.boxX, box5.boxY, box5.boxWidth, box5.boxHeight);
             }
-            if(score > 10) {
+            if (score > 10) {
                 rect(box6.boxX, box6.boxY, box6.boxWidth, box6.boxHeight);
             }
 
-
-            score+= 0.01;
+            score += 0.01;
             textAlign(CENTER);
             textFont(font, 150);
-            text((int)score, 250, 150);
-            if(score == 5) {
+            text((int) score, 250, 150);
+            if (score == 5) {
                 box5 = new BouncingBox();
             }
-            if(score == 10) {
+            if (score == 10) {
                 box6 = new BouncingBox();
             }
-            if(shake) {
+            if (shake) {
                 popMatrix();
             }
-            if(MODE == "PAUSE") {
+            if (MODE == "PAUSE") {
                 fill(255, 255, 255);
                 rect(box1.boxX, box1.boxY, box1.boxWidth, box1.boxHeight);
                 rect(box2.boxX, box2.boxY, box2.boxWidth, box2.boxHeight);
                 rect(box3.boxX, box3.boxY, box3.boxWidth, box3.boxHeight);
                 rect(box4.boxX, box4.boxY, box4.boxWidth, box4.boxHeight);
-                fill(255,255,255);
-                if(score > 5) {
+                fill(255, 255, 255);
+                if (score > 5) {
                     rect(box5.boxX, box5.boxY, box5.boxWidth, box5.boxHeight);
                 }
-                if(score > 10) {
+                if (score > 10) {
                     rect(box6.boxX, box6.boxY, box6.boxWidth, box6.boxHeight);
                 }
                 fill(255, 0, 0);
                 rect(playerX, 470, 30, 30);
             }
-        } else if(MODE == "PAUSE") {
+        } else if (MODE == "PAUSE") {
             screenSlideX = 0;
             delay(1000);
             MODE = "SLIDE";
         }
-        if(MODE == "SLIDE") {
+        if (MODE == "SLIDE") {
             screenSlideX += (500 - screenSlideX) / 30;
             background(255, 255, 255);
 
             pushMatrix();
             translate(screenSlideX, 0);
 
-            fill(0,0,0);
-            rect(0,0,width,height);
+            fill(0, 0, 0);
+            rect(0, 0, width, height);
 
             fill(255, 255, 255);
             rect(box1.boxX, box1.boxY, box1.boxWidth, box1.boxHeight);
@@ -234,18 +232,18 @@ public class MyFirstProject extends PApplet{
             rect(box4.boxX, box4.boxY, box4.boxWidth, box4.boxHeight);
             fill(255, 0, 0);
             rect(playerX, 470, 30, 30);
-            fill(255,255,255);
-            if(score > 5) {
+            fill(255, 255, 255);
+            if (score > 5) {
                 rect(box5.boxX, box5.boxY, box5.boxWidth, box5.boxHeight);
             }
-            if(score > 10) {
+            if (score > 10) {
                 rect(box6.boxX, box6.boxY, box6.boxWidth, box6.boxHeight);
             }
             textAlign(CENTER);
             textFont(font, 150);
-            text((int)score, 250, 150);
+            text((int) score, 250, 150);
             popMatrix();
-            if(round(screenSlideX) == 500) {
+            if (round(screenSlideX) == 500) {
                 MODE = "GAME OVER";
                 overSlideY = -150;
                 scoreSlideY = 1000;
@@ -253,64 +251,65 @@ public class MyFirstProject extends PApplet{
                 menuButtonSlideY = 15000;
             }
         }
-        if(MODE == "GAME OVER") {
+        if (MODE == "GAME OVER") {
             overSlideY += (0 - overSlideY) / 10;
-            scoreSlideY += (0-scoreSlideY / 10);
+            scoreSlideY += (0 - scoreSlideY / 10);
             score2SlideY += (0 - score2SlideY / 10);
             menuButtonSlideY += (0 - menuButtonSlideY) / 10;
             background(255, 255, 255);
-            fill(0,0,0);
+            fill(0, 0, 0);
             textAlign(CENTER);
             textFont(font, 100);
 
             pushMatrix();
-            translate(0,overSlideY);
+            translate(0, overSlideY);
             text("GAME OVER", 250, 150);
             popMatrix();
 
             textFont(font, 75);
             pushMatrix();
-            translate(0,scoreSlideY);
+            translate(0, scoreSlideY);
             text("Score:", 250, 250);
             popMatrix();
 
             textFont(font, 70);
             pushMatrix();
-            translate(0,score2SlideY);
-            text((int)score, 250, 350);
+            translate(0, score2SlideY);
+            text((int) score, 250, 350);
             popMatrix();
 
             pushMatrix();
-            translate(0,menuButtonSlideY);
-            if(mouseX < 310 && mouseX > 190 && mouseY > 395 && mouseY < 445) {
-                fill(0,0,0);
-                rect(190,395,120,50);
-                fill(255,255,255);
+            translate(0, menuButtonSlideY);
+            if (mouseX < 310 && mouseX > 190 && mouseY > 395 && mouseY < 445) {
+                fill(0, 0, 0);
+                rect(190, 395, 120, 50);
+                fill(255, 255, 255);
                 textFont(font, 50);
                 text("MENU", 250, 440);
-                if(mousePressed) {
+                if (mousePressed) {
                     delay(300);
                     setup();
                 }
             } else {
-                fill(0,0,0);
+                fill(0, 0, 0);
                 textFont(font, 50);
                 text("MENU", 250, 440);
             }
             popMatrix();
         }
 
-
-        //rect(mouseX, mouseY, 20, 20);
+        // rect(mouseX, mouseY, 20, 20);
         /*
-        textFont(font, 50);
-        fill(200,100,0);
-        String text1 = "X = " + mouseX + ", Y = " + mouseY;
-        text(text1, mouseX, mouseY);
-        */
+         * textFont(font, 50);
+         * fill(200,100,0);
+         * String text1 = "X = " + mouseX + ", Y = " + mouseY;
+         * text(text1, mouseX, mouseY);
+         */
 
+    }
 
-
+    public static void main(String[] args) {
+        PApplet.main("MyFirstProject");
     }
 
 }
